1d498b20057d9340e04a5b0fadf3f67b5ba46889
Dionisii Grinko <DionisiyGr@gmail.com>
2025-12-01 23:06:57 -0500

    add executin statistics

--- cmd/ipchecker/main.go
+++ cmd/ipchecker/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"runtime"
+	"time"
 
 	"github.com/DionisiyGri/ipv4-checker/internal/ipchecker"
 )
@@ -18,6 +21,8 @@ func main() {
 		os.Exit(2)
 	}
 
+	startTime := time.Now()
+
 	//run the logic to check and count IPs
 	res, err := ipchecker.Execute(*path)
 	if err != nil {
@@ -25,6 +30,21 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Lines processed: %d\n", res.Lines)
-	fmt.Printf("Unique addresses: %d\n", res.Unique)
+	log.Printf("Lines processed: %d\n", res.Lines)
+	log.Printf("Unique addresses: %d\n", res.Unique)
+
+	printMemStats()
+	fmt.Printf("Processing completed in %v", time.Since(startTime))
+}
+
+func printMemStats() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+
+	log.Printf("Memory stats: Alloc=%.2fMB, TotalAlloc=%.2fMB, Sys=%.2fMB, NumGC=%d",
+		float64(m.Alloc)/1024/1024,
+		float64(m.TotalAlloc)/1024/1024,
+		float64(m.Sys)/1024/1024,
+		m.NumGC,
+	)
 }
